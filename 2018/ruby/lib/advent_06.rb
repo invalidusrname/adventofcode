7490be106ab8db0f509627a3fea531a6fa16003c
Matt McMahand <mmcmahand@gmail.com>
2018-12-25 17:56:02 -0500

    day 6

---
+++ 2018/ruby/lib/advent_06.rb
@@ -0,0 +1,187 @@
+class Coordinate
+
+  attr_reader :x, :y, :count
+  attr_writer :count
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @count = 1
+  end
+
+  def to_s
+    name
+  end
+
+  def name
+    "(#{x},#{y})"
+  end
+end
+
+class ChronalCoordinates
+
+  attr_reader :coordinates
+
+  def initialize(coordinates)
+    @coordinates = coordinates
+
+    @grid = Hash.new
+    @counts = Hash.new(0)
+
+    fill_grid
+    fill_closest
+    fill_counts
+  end
+
+  def min_x
+    @min_x ||= @coordinates.collect { |c| c.x }.min
+  end
+
+  def max_x
+    @max_x ||= @coordinates.collect { |c| c.x }.max
+  end
+
+  def min_y
+    @min_y ||= @coordinates.collect { |c| c.y }.min
+  end
+
+  def max_y
+    @max_y ||= @coordinates.collect { |c| c.y }.max
+  end
+
+  def infinite_coordinate?(coordinate)
+    x = coordinate.x
+    y = coordinate.y
+
+    ((min_x == x) || (max_x == x) || (min_y == y) || (max_y == y))
+  end
+
+  def infinite_locations
+    locations = []
+
+    (min_x..max_x).each do |x|
+      (min_y..max_y).each do |y|
+        point = Coordinate.new(x,y)
+        closest = @grid[point.to_s]
+        if closest.is_a?(Coordinate)
+          locations << closest if infinite_coordinate?(point)
+        end
+      end
+    end
+
+    locations.uniq
+  end
+
+  def largest_area
+    infinite = infinite_locations
+
+    allowed = @counts.reject do |c, total|
+      infinite.any? { |i| i.x == c.x && i.y == c.y }
+    end
+
+    @counts.collect do |coordinate, total|
+      if allowed.include?(coordinate)
+        total
+      end
+    end.flatten.compact.max
+  end
+
+  def get_coordinate(point)
+    @coordinates.detect { |c| c.x == point.x && c.y == point.y }
+  end
+
+  def manhattan_distance(p, q)
+    (p.x - q.x).abs + (p.y - q.y).abs
+  end
+
+  def fill_grid
+    (min_x..max_x).each do |x|
+      (min_y..max_y).each do |y|
+        point = Coordinate.new(x,y)
+        @grid[point.to_s] = get_coordinate(point) || '.'
+      end
+    end
+  end
+
+  def fill_closest
+    (min_x..max_x).each do |x|
+      (min_y..max_y).each do |y|
+        point = Coordinate.new(x,y)
+
+        next if @grid[point.to_s].is_a?(Coordinate)
+        coordinate = closest_coordinate(point)
+
+        if coordinate
+          @grid[point.to_s] = coordinate
+          # puts "NEAREST [#{point}] => #{coordinate.name}"
+        end
+      end
+    end
+  end
+
+  def fill_counts
+    @grid.each do |key, value|
+      if value.is_a?(Coordinate)
+        @counts[value] += 1
+      end
+    end
+  end
+
+  def closest_coordinate(point)
+    nearest_distance = nil
+    nearest = nil
+
+    # puts "SCANNING: #{point}"
+
+    @coordinates.each do |c|
+      distance = manhattan_distance(point, c)
+
+      # puts "CHECKING: #{c} distance #{distance}"
+
+      if nearest_distance.nil?
+        nearest_distance = distance
+        nearest = c
+      elsif distance < nearest_distance
+        nearest_distance = distance
+        nearest = c
+      elsif distance == nearest_distance
+        nearest = nil
+      end
+    end
+
+    # puts "NEAREST -> #{nearest}"
+
+    nearest
+  end
+
+  def print_grid
+    puts "(#{min_x},#{min_y}) <> (#{max_x},#{max_y})"
+
+    (min_x..max_x).each do |x|
+      (min_y..max_y).each do |y|
+        point = Coordinate.new(x, y)
+        value = @grid[point.to_s].to_s
+        print "#{value} "
+      end
+      puts
+    end
+  end
+
+  def largest_region
+    count = 0
+
+    (min_x..max_x).each do |x|
+      (min_y..max_y).each do |y|
+        point = Coordinate.new(x,y)
+
+        total = @coordinates.inject(0) do |sum, c|
+          sum + manhattan_distance(point, c)
+        end
+
+        count += 1 unless total >= 10000
+      end
+    end
+
+    count
+  end
+end
---
+++ 2018/ruby/spec/advent_06_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+require 'advent_06'
+
+describe 'ChronalCoordinates' do
+  let(:puzzle_input) do
+    File.readlines('spec/fixtures/advent-06.txt').map do |line|
+      line.chomp.split(',').collect(&:to_i)
+    end
+  end
+
+  let(:sample_input) do
+    [
+      [1,1],
+      [1,6],
+      [8,3],
+      [3,4],
+      [5,5],
+      [8,9],
+    ]
+  end
+
+  it "reduces some sample input" do
+    coordinates = sample_input.collect do |item|
+      Coordinate.new(item[0], item[1])
+    end
+
+    c = ChronalCoordinates.new(coordinates)
+    # c.print_grid
+
+    expect(c.largest_area).to eq(17)
+  end
+
+  xit "solves part 1" do
+    coordinates = puzzle_input.collect do |item|
+      Coordinate.new(item[0], item[1])
+    end
+
+    c = ChronalCoordinates.new(coordinates)
+    # c.print_grid
+
+    expect(c.largest_area).to eq(3401)
+  end
+
+  it "solves part 2" do
+    coordinates = puzzle_input.collect do |item|
+      Coordinate.new(item[0], item[1])
+    end
+
+    c = ChronalCoordinates.new(coordinates)
+    expect(c.largest_region).to eq(49327)
+  end
+end
